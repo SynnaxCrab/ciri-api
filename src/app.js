ce6a8afcdf25bb6eef5785929006a31a149c3ac9
Jeff Lai <winfield301@gmail.com>
2016-12-05 14:29:57 +0800

    Add basic project structure

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import Koa from 'koa'
+import mount from 'koa-mount'
 
 const app = new Koa()
+const api = new Koa()
 
+api.use(async ctx => {
+  ctx.body = "Hello Koa API!"
+})
+
+app.use(mount('/api', api))
 app.use(async ctx => {
   ctx.body = "Hello Koa!"
 })
