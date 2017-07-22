8ba17fd6956486d126fd1f6134d1a838a90e6610
Jeff Lai <winfield301@gmail.com>
2017-07-22 21:17:46 +0800

    Integrate with optics agent

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ import cors from 'kcors'
 import koaRouter from 'koa-router'
 import koaBody from 'koa-bodyparser'
 import { graphqlKoa, graphiqlKoa } from 'graphql-server-koa'
+import OpticsAgent from 'optics-agent'
 
 import schema from './data/schema'
+OpticsAgent.instrumentSchema(schema)
 
 const app = new Koa()
 const router = new koaRouter()
@@ -12,8 +14,22 @@ const PORT = 3000
 
 app.use(cors())
 app.use(koaBody())
+app.use(OpticsAgent.koaMiddleware())
 
-router.post('/graphql', graphqlKoa({ schema: schema }))
+router.post(
+  '/graphql',
+  graphqlKoa(async ctx => {
+    // create an optic context
+    const opticsContext = OpticsAgent.context(ctx.request);
+    // create a context for each request
+    const context = { opticsContext };
+    return {
+      schema: schema,
+      context,
+    };
+  })
+);
+
 router.get('/graphql', graphqlKoa({ schema: schema }))
 
 router.get('/graphiql', graphiqlKoa({
