5bad0e37ddff5c779f487c9c778760198388fb31
Jeff Lai <winfield301@gmail.com>
2017-07-26 12:48:04 +0800

    Fix config/database.js

--- src/config/database.js
+++ src/config/database.js
@@ -10,14 +10,14 @@ module.exports = {
   },
   test: {
     database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     dialect: 'postgres',
   },
   production: {
     database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     dialect: 'postgres',
