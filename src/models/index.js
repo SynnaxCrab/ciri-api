c5c5699c4928de3ac097b14bd6a3621dc161d563
Jeff Lai <winfield301@gmail.com>
2017-11-27 21:43:53 +0800

    Add debug info to debug database connection with RDS

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,8 @@ import fs from 'fs'
 import path from 'path'
 import Sequelize from 'sequelize'
 
+console.log(`DB_NAME: ${process.env.DB_NAME}`)
+console.log(`DB_HOST: ${process.env.DB_HOST}`)
 const basename  = path.basename(module.filename)
 const sequelize = new Sequelize(
   process.env.DB_NAME,
