959c1c6fd084eab460281f8ce5c5a82fca4f0e6e
Jeff Lai <winfield301@gmail.com>
2017-11-27 22:08:56 +0800

    Delegate sequelize to use node postgres ENV

--- src/models/index.js
+++ src/models/index.js
@@ -2,15 +2,11 @@ import fs from 'fs'
 import path from 'path'
 import Sequelize from 'sequelize'
 
-console.log(`DB_NAME: ${process.env.DB_NAME}`)
-console.log(`DB_HOST: ${process.env.DB_HOST}`)
 const basename  = path.basename(module.filename)
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.PGHOST,
+    port: 5432,
     dialect: 'postgres'
   }
 )
