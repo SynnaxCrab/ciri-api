f31aa4a936c27f544233a34eb52ac4632d84a720
Jeff Lai <winfield301@gmail.com>
2017-12-02 14:16:02 +0800

    Fix up deploy script env echos

--- up-deploy.sh
+++ up-deploy.sh
@@ -1,8 +1,8 @@
 yarn build:up 
 cd up
 echo PGHOST=$PGHOST > .env
-echo PGUSER=$PGUSER > .env
-echo PGPASSWORD=$PGPASSWORD > .env
-echo PGDATABASE=$PGDATABASE > .env
+echo PGUSER=$PGUSER >> .env
+echo PGPASSWORD=$PGPASSWORD >> .env
+echo PGDATABASE=$PGDATABASE >> .env
 yarn install --prod 
 ../bin/up deploy production
