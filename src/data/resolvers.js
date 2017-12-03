5d7394054bc09144ededab3c1339c44550bb60ec
Jeff Lai <winfield301@gmail.com>
2017-12-03 21:41:36 +0800

    Rewrite resolvers using pure pg

--- src/data/resolvers.js
+++ src/data/resolvers.js
@@ -1,21 +1,43 @@
-import models from '../models'
-
-const Article = models.Article
+import { sql } from 'pg-sql'
+import { INSERT, WHERE } from 'pg-sql-helpers'
+import { pool } from '../db'
+import uuidv4 from 'uuid/v4'
 
 const resolveFunctions = {
   Query: {
     articles: async() => {
-      return await Article.scope('recent').findAll()
+      try {
+        const res = await pool.query(sql`
+          SELECT *
+          FROM articles
+          LIMIT 5
+        `)
+        return res.rows
+      } catch(err) {
+        console.log(err.stack)
+      }
     }
   },
   Mutation: {
     createArticle: async (_, data) => {
-      return await Article.create(data)
+      const { title } = data
+      const id = uuidv4()
+      const slug = title.toLowerCase().split(' ').join('-')
+      const now = new Date()
+      try {
+        const res = await pool.query(sql`
+          ${INSERT('articles', { ...data, id: id, slug: slug, createdAt: now, updatedAt: now })}
+          RETURNING *
+        `)
+      } catch(err) {
+        console.log(err.stack)
+      }
     },
     destroyArticle: async (_, { id }) => {
-      const article = await Article.findById(id)
-      await article.destroy()
-      return article
+      await pool.query(sql`
+        DELETE FROM articles
+        ${WHERE({ id: id })}
+      `)
     }
   }
 }
