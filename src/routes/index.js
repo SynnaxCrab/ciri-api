6611eb0357045629d8edea3ba288f7ae59c56606
Jeff Lai <winfield301@gmail.com>
2017-12-03 17:47:33 +0800

    Remove unused code for sequelize

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import router from 'koa-router'
-import * as articles from '../controllers/articles'
-
-export default router()
-  .get('/articles', articles.index())
-  .get('/articles/:slug', articles.show())
-  .post('/articles', articles.create())
-  .put('/articles/:slug', articles.update())
