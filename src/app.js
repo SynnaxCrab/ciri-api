b41530b07fa12ba11efd4e16bda06e89bb5e505c
Jeff Lai <winfield301@gmail.com>
2018-01-26 23:24:15 +0800

    Add passport twitter strategy integration

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import koaRouter from "koa-router"
 import bodyParser from "koa-bodyparser"
 import session from "koa-session"
 import passport from "koa-passport"
+import twitterStrategy from "passport-twitter"
 import { graphqlKoa, graphiqlKoa } from "apollo-server-koa"
 
 import schema from "./schema"
@@ -18,6 +19,21 @@ const { PORT = 3000 } = process.env
 router.post("/graphql", koaBody(), graphqlKoa({ schema }))
 router.get("/graphql", graphqlKoa({ schema }))
 router.get("/graphiql", graphiqlKoa({ endpointURL: "/graphql" }))
+
+passport.use(
+  new twitterStrategy(
+    {
+      consumerKey: TWITTER_CONSUMER_KEY,
+      consumerSecret: TWITTER_CONSUMER_SECRET,
+      callbackURL: "http://127.0.0.1:3000/auth/twitter/callback",
+    },
+    function(token, tokenSecret, profile, cb) {
+      User.findOrCreate({ twitterId: profile.id }, function(err, user) {
+        return cb(err, user)
+      })
+    },
+  ),
+)
 
 app.use(router.routes())
 app.use(router.allowedMethods())
