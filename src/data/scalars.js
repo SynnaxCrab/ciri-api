abaa446e2f963776f5649e91ad60fd2a565e9525
Jeff Lai <winfield301@gmail.com>
2018-01-29 00:56:25 +0800

    Add DateTime scalar type resolver

--- src/data/scalars.js
+++ src/data/scalars.js
@@ -1,3 +1,25 @@
-export default `
-  scalar Date
+import { GraphQLScalarType } from "graphql"
+import { Kind } from "graphql/language"
+
+export const typeDefs = `
+  scalar DateTime
 `
+
+const resolvers = {
+  Date: new GraphQLScalarType({
+    name: "Date",
+    description: "Date custom scalar type",
+    parseValue(value) {
+      return new Date(value) // value from the client
+    },
+    serialize(value) {
+      return value.getTime() // value sent to the client
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return parseInt(ast.value, 10) // ast value is always in string format
+      }
+      return null
+    },
+  }),
+}
