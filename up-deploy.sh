ec7d283d843b628b229a92da605e2b17de71e08e
Jeff Lai <winfield301@gmail.com>
2018-07-16 15:00:58 +0800

    Remove old up config files

--- up-deploy.sh
+++
@@ -1,9 +0,0 @@
-yarn build:up
-cd up
-echo PGHOST=$PGHOST > .env
-echo PGUSER=$PGUSER >> .env
-echo PGPASSWORD=$PGPASSWORD >> .env
-echo PGDATABASE=$PGDATABASE >> .env
-yarn install --prod
-node-prune
-up deploy production
