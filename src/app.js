4bc265c2b502d0b0f36d90d3c94a90b3c81c22f6
Jeff Lai <winfield301@gmail.com>
2018-07-22 16:31:51 +0800

    Remove auth temporary

--- src/app.js
+++ src/app.js
@@ -13,12 +13,13 @@ const server = new ApolloServer({
 })
 
 const app = new Koa()
-server.applyMiddleware({ app })
 const { PORT = 3000 } = process.env
 
 app.keys = [process.env.SECRET]
 app.use(cors({ credentials: true }))
-//app.use(auth)
+// FIXME: Fix auth with GraphQL Playground
+// app.use(auth)
+server.applyMiddleware({ app })
 
 console.log(`App started on PORT: ${PORT}`)
 app.listen(PORT)
