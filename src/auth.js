6a915c708ff2b8d4ef28569fcdf93c216200eafb
Jeff Lai <winfield301@gmail.com>
2018-09-03 16:00:23 +0800

    Bypass playground in auth middleware

--- src/auth.js
+++ src/auth.js
@@ -2,6 +2,11 @@ import jwt from 'jsonwebtoken'
 import { findUserByUuid } from './db/user'
 
 const auth = async (ctx, next) => {
+  if (ctx.path === '/playground') {
+    await next()
+    return
+  }
+
   const accessToken = ctx.cookies.get('access_token', { signed: true })
   try {
     const payload = jwt.verify(accessToken, process.env.SECRET)
